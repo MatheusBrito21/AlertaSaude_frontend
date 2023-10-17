5cd62aefd7647e1c6fe489d198c03cc008b86a06
MatheusBrito21 <matheusb790@gmail.com>
2023-10-17 15:31:57 -0300

    design responsivo ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from './component/home/home.component';
 import { NavComponent } from './component/nav/nav.component';
 import { SobreComponent } from './component/sobre/sobre.component';
 import { DoencasCronicasComponent } from './component/doencas-cronicas/doencas-cronicas.component';
+import { ObjetivosComponent } from './component/objetivos/objetivos.component';
+import { ComoFuncionaComponent } from './component/como-funciona/como-funciona.component';
+import { DiferenciaisComponent } from './component/diferenciais/diferenciais.component';
+import { RepresentanteComponent } from './component/representante/representante.component';
 
 const routes: Routes = [
   {path:'', component:NavComponent , 
@@ -11,6 +15,10 @@ const routes: Routes = [
       {path:'home' , component: HomeComponent},
       {path:'sobre' , component: SobreComponent},
       {path:'doencas' , component: DoencasCronicasComponent},
+      {path:'objetivos' , component: ObjetivosComponent},
+      {path:'como-funciona' , component: ComoFuncionaComponent},
+      {path:'diferenciais' , component: DiferenciaisComponent},
+      {path:'representante' , component: RepresentanteComponent},
 
     ] }
   
